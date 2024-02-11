08b735efee7181785d20147475c9e36b800f1655
Samuel Levy <samulevy@gmail.com>
2024-02-10 21:17:02 -0300

    WIP

--- lib/extep.ex
+++ lib/extep.ex
@@ -41,24 +41,39 @@ defmodule Extep do
   def new(context) when is_map(context), do: %Extep{context: context}
 
   @doc """
-  Executes the given function and sets its return to an index key in the context.
+  Refer to run/3
   """
   @spec run(t(), ctx_mod_fun()) :: t()
-  def run(%Extep{status: :ok, last_step_idx: nil} = extep, fun), do: run(extep, 0, fun)
-  def run(%Extep{status: :ok} = extep, fun), do: run(extep, extep.last_step_idx + 1, fun)
-  def run(%Extep{status: status} = extep, _fun) when is_halted(status), do: extep
+  def run(extep, fun), do: run(extep, fun, [])
+
+  @doc """
+  Executes the given function and sets its return to an index key in the context.
+  """
+  @spec run(t(), ctx_mod_fun(), keyword()) :: t()
+  def run(%Extep{status: :ok, last_step_idx: nil} = extep, fun, opts) when is_function(fun, 1) do
+    run(extep, 0, fun, opts)
+  end
+
+  def run(%Extep{status: :ok} = extep, fun, opts) when is_function(fun, 1) do
+    run(extep, extep.last_step_idx + 1, fun, opts)
+  end
+
+  def run(%Extep{status: status} = extep, _fun, _opts) when is_halted(status), do: extep
 
   @doc """
   Executes the given function and sets its return to the given context key.
   """
-  @spec run(t(), ctx_key(), ctx_mod_fun()) :: t()
-  def run(%Extep{status: :ok, context: context} = extep, ctx_key, fun) do
+  @spec run(t(), ctx_key(), ctx_mod_fun(), keyword()) :: t()
+  def run(extep, ctx_key, fun, opts \\ [])
+
+  def run(%Extep{status: :ok, context: context} = extep, ctx_key, fun, opts)
+      when is_ctx_key(ctx_key) and is_function(fun, 1) do
     context
     |> fun.()
-    |> update_extep(extep, ctx_key)
+    |> update_extep(extep, ctx_key, opts)
   end
 
-  def run(%Extep{status: status} = extep, _ctx_key, _fun) when is_halted(status), do: extep
+  def run(%Extep{status: status} = extep, _ctx_key, _fun, _opts) when is_halted(status), do: extep
 
   @doc """
   Returns the value of the last step
@@ -85,22 +100,36 @@ defmodule Extep do
     handle_return(extep, extep.last_step, opts)
   end
 
-  @spec update_extep({return_tag(), any()}, t(), ctx_key()) :: t()
-  defp update_extep({tag, value}, %Extep{} = extep, ctx_key) when is_valid_return_tag(tag) do
+  @spec update_extep({return_tag(), any()}, t(), ctx_key(), keyword()) :: t()
+  defp update_extep({tag, value}, %Extep{} = extep, ctx_key, opts)
+       when is_valid_return_tag(tag) do
+    status = Map.get(@tag_status_dict, tag)
+
     Map.merge(extep, %{
-      status: Map.get(@tag_status_dict, tag),
-      context: Map.put(extep.context, ctx_key, value),
+      status: status,
+      context: update_context(extep.context, status, ctx_key, value, opts),
       last_step: ctx_key,
       last_step_idx: handle_idx(extep.last_step_idx)
     })
   end
 
-  @spec update_extep(return_tag(), t(), ctx_key()) :: t()
-  defp update_extep(tag, %Extep{} = extep, ctx_key) when is_valid_return_tag(tag) do
-    update_extep({tag, tag}, extep, ctx_key)
+  @spec update_extep(return_tag(), t(), ctx_key(), keyword()) :: t()
+  defp update_extep(tag, %Extep{} = extep, ctx_key, opts) when is_valid_return_tag(tag) do
+    update_extep({tag, tag}, extep, ctx_key, opts)
   end
 
-  defp update_extep(_tag, _extep, _ctx_key), do: raise(Extep.InvalidFunctionReturn)
+  defp update_extep(_tag, _extep, _ctx_key, _opts), do: raise(Extep.InvalidFunctionReturn)
+
+  defp update_context(context, :ok, ctx_key, value, opts) do
+    case Keyword.get(opts, :set, ctx_key) do
+      ^ctx_key -> Map.put(context, ctx_key, value)
+      set -> context |> Map.put(set, value) |> Map.put(ctx_key, {:set_to, set})
+    end
+  end
+
+  defp update_context(context, _status, ctx_key, value, _opts) do
+    Map.put(context, ctx_key, value)
+  end
 
   @spec handle_return(t(), ctx_key(), opts()) :: return_type()
   defp handle_return(%Extep{status: status, context: context}, ctx_key, opts) do
--- test/extep_test.exs
+++ test/extep_test.exs
@@ -32,6 +32,17 @@ defmodule ExtepTest do
              }
     end
 
+    test "given the `set: :ctx_key` option, sets the return of the function to the given context key" do
+      extep = %Extep{status: :ok, context: %{}, last_step: nil, last_step_idx: nil}
+
+      assert Extep.run(extep, fn _context -> :ok end, set: :foo) == %Extep{
+               status: :ok,
+               context: %{0 => {:set_to, :foo}, :foo => :ok},
+               last_step: 0,
+               last_step_idx: 0
+             }
+    end
+
     test "executes next steps when function returns `:ok`" do
       extep = %Extep{
         status: :ok,
@@ -53,7 +64,7 @@ defmodule ExtepTest do
              }
     end
 
-    test "when function returns an `:ok` tuple" do
+    test "when function returns an `{:ok, _}` tuple" do
       extep = %Extep{
         status: :ok,
         context: %{:initial => "value", 0 => "first"},
@@ -69,7 +80,7 @@ defmodule ExtepTest do
              }
     end
 
-    test "executes next steps when function returns an `:ok` tuple" do
+    test "executes next steps when function returns an `{:ok, _}` tuple" do
       extep = %Extep{
         status: :ok,
         context: %{:initial => "value", 0 => "first"},
@@ -106,6 +117,22 @@ defmodule ExtepTest do
              }
     end
 
+    test "ignores `set: :ctx_key` option when function returns `:halt`" do
+      extep = %Extep{
+        status: :ok,
+        context: %{:initial => "value", 0 => "first"},
+        last_step: 0,
+        last_step_idx: 0
+      }
+
+      assert Extep.run(extep, fn _context -> :halt end, set: :foo) == %Extep{
+               status: :halted,
+               context: %{:initial => "value", 0 => "first", 1 => :halt},
+               last_step: 1,
+               last_step_idx: 1
+             }
+    end
+
     test "skips next steps when function returns `:halt`" do
       extep = %Extep{
         status: :ok,
@@ -127,7 +154,7 @@ defmodule ExtepTest do
              }
     end
 
-    test "when function returns a `:halt` tuple" do
+    test "when function returns a `{:halt, _}` tuple" do
       extep = %Extep{
         status: :ok,
         context: %{:initial => "value", 0 => "first"},
@@ -143,7 +170,7 @@ defmodule ExtepTest do
              }
     end
 
-    test "skips next steps when function returns a `:halt` tuple" do
+    test "skips next steps when function returns a `{:halt, _}` tuple" do
       extep = %Extep{
         status: :ok,
         context: %{:initial => "value", 0 => "first"},
@@ -180,6 +207,22 @@ defmodule ExtepTest do
              }
     end
 
+    test "ignores `set: :ctx_key` option when function returns `:error`" do
+      extep = %Extep{
+        status: :ok,
+        context: %{:initial => "value", 0 => "first"},
+        last_step: 0,
+        last_step_idx: 0
+      }
+
+      assert Extep.run(extep, fn _context -> :error end, set: :foo) == %Extep{
+               status: :error,
+               context: %{:initial => "value", 0 => "first", 1 => :error},
+               last_step: 1,
+               last_step_idx: 1
+             }
+    end
+
     test "skips next steps when function returns `:error`" do
       extep = %Extep{
         status: :ok,
@@ -201,7 +244,7 @@ defmodule ExtepTest do
              }
     end
 
-    test "when function returns a `:error` tuple" do
+    test "when function returns a `{:erorr, _}` tuple" do
       extep = %Extep{
         status: :ok,
         context: %{:initial => "value", 0 => "first"},
@@ -217,7 +260,7 @@ defmodule ExtepTest do
              }
     end
 
-    test "skips next steps when function returns a `:error` tuple" do
+    test "skips next steps when function returns a `{:erorr, _}` tuple" do
       extep = %Extep{
         status: :ok,
         context: %{:initial => "value", 0 => "first"},
@@ -254,6 +297,17 @@ defmodule ExtepTest do
       assert Extep.run(extep, :first, fn _context -> :ok end) == %Extep{
                status: :ok,
                context: %{:first => :ok},
+               last_step: :first,
+               last_step_idx: 0
+             }
+    end
+
+    test "given the `set: :ctx_key` option, sets the return of the function to the given context key" do
+      extep = %Extep{status: :ok, context: %{}, last_step: nil, last_step_idx: nil}
+
+      assert Extep.run(extep, :first, fn _context -> :ok end, set: :foo) == %Extep{
+               status: :ok,
+               context: %{:first => {:set_to, :foo}, :foo => :ok},
                last_step: :first,
                last_step_idx: 0
              }
@@ -354,6 +408,22 @@ defmodule ExtepTest do
              }
     end
 
+    test "ignores `set: :ctx_key` option when function returns `:halt`" do
+      extep = %Extep{
+        status: :ok,
+        context: %{:initial => "value", 0 => "first"},
+        last_step: 0,
+        last_step_idx: 0
+      }
+
+      assert Extep.run(extep, :second, fn _context -> :halt end, set: :foo) == %Extep{
+               status: :halted,
+               context: %{:initial => "value", 0 => "first", :second => :halt},
+               last_step: :second,
+               last_step_idx: 1
+             }
+    end
+
     test "skips next steps when function returns `:halt`" do
       extep = %Extep{
         status: :ok,
@@ -384,6 +454,22 @@ defmodule ExtepTest do
       }
 
       assert Extep.run(extep, :second, fn _context -> {:halt, "halted in second"} end) == %Extep{
+               status: :halted,
+               context: %{:initial => "value", 0 => "first", :second => "halted in second"},
+               last_step: :second,
+               last_step_idx: 1
+             }
+    end
+
+    test "ignores `set: :ctx_key` option when function returns a `:halt` tuple" do
+      extep = %Extep{
+        status: :ok,
+        context: %{:initial => "value", 0 => "first"},
+        last_step: 0,
+        last_step_idx: 0
+      }
+
+      assert Extep.run(extep, :second, fn _context -> {:halt, "halted in second"} end, set: :foo) == %Extep{
                status: :halted,
                context: %{:initial => "value", 0 => "first", :second => "halted in second"},
                last_step: :second,
@@ -428,6 +514,22 @@ defmodule ExtepTest do
              }
     end
 
+    test "ignores `set: :ctx_key` option when function returns `:error`" do
+      extep = %Extep{
+        status: :ok,
+        context: %{:initial => "value", 0 => "first"},
+        last_step: 0,
+        last_step_idx: 0
+      }
+
+      assert Extep.run(extep, :second, fn _context -> :error end, set: :foo) == %Extep{
+               status: :error,
+               context: %{:initial => "value", 0 => "first", :second => :error},
+               last_step: :second,
+               last_step_idx: 1
+             }
+    end
+
     test "skips next steps when function returns `:error`" do
       extep = %Extep{
         status: :ok,
@@ -465,6 +567,22 @@ defmodule ExtepTest do
              }
     end
 
+    test "ignores `set: :ctx_key` option when function returns an `{:error, _}` tuple" do
+      extep = %Extep{
+        status: :ok,
+        context: %{:initial => "value", 0 => "first"},
+        last_step: 0,
+        last_step_idx: 0
+      }
+
+      assert Extep.run(extep, :second, fn _context -> {:error, "error in second"} end, set: :foo) == %Extep{
+               status: :error,
+               context: %{:initial => "value", 0 => "first", :second => "error in second"},
+               last_step: :second,
+               last_step_idx: 1
+             }
+    end
+
     test "skips next steps when function returns a `:error` tuple" do
       extep = %Extep{
         status: :ok,
@@ -484,9 +602,6 @@ defmodule ExtepTest do
                last_step: :second,
                last_step_idx: 1
              }
-    end
-
-    test "when context key is an atom" do
     end
 
     test "raises `Extep.InvalidFunctionReturn` when function return is invalid" do
