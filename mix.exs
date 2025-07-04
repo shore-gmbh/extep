05505541bf246e7ff17fbc0faf0c0004397cb5c9
Samuel Levy <samulevy@gmail.com>
2025-07-04 19:03:35 -0300

    Bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Extep.MixProject do
     [
       app: :extep,
       name: "Extep",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: [
